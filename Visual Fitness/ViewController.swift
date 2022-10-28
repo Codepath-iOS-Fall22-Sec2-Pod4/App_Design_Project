99b1fe8c997e0a0531ea8d188a95d2eb8c8bb0a9
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2022-10-28 11:24:54 -0400

    initial commit

---
+++ Visual Fitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Visual Fitness
+//
+//  Created by Krina Patel on 10/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
