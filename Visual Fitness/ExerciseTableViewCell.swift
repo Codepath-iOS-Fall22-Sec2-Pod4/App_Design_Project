ee18b131663433e93b28b775abf4af7630fdcb3f
Kaidi1 <winson096@gmail.com>
2022-11-15 01:54:15 -0500

    added a popup showing exercises after pressing on body part

---
+++ Visual Fitness/ExerciseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExerciseTableViewCell.swift
+//  Visual Fitness
+//
+//  Created by Kaidi Gao on 11/14/22.
+//
+
+import UIKit
+
+class ExerciseTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var exerciseCell: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
